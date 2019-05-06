5eb52f177c3e9f8ef5e665ff1003e489f0705fbc
LoveChase <info@chaselove.tech>
2019-05-06 02:03:33 -0500

    Updates

---
+++ precache-manifest.7b1983d5cc50d97d6c8188f25e1d7459.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7e9b674060ede50ac63fc3019f2d450",
+    "url": "/static/media/Draw.f7e9b674.mp3"
+  },
+  {
+    "revision": "8502a74c4a4f0a4fd6d0",
+    "url": "/static/css/main.ae9d921e.chunk.css"
+  },
+  {
+    "revision": "a86cf3805d980b9d56b2",
+    "url": "/static/js/1.a86cf380.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "545976ff505aa351285083190a9d9401",
+    "url": "/static/media/fireAvatar.545976ff.jpg"
+  },
+  {
+    "revision": "31709afa014ad38c7f5c1756fe794628",
+    "url": "/static/media/grassAvatar.31709afa.jpg"
+  },
+  {
+    "revision": "b67e6ed2413c113750977f1bfd62e549",
+    "url": "/static/media/shield.b67e6ed2.png"
+  },
+  {
+    "revision": "8e7c6c628e9b9dd6aeba75b205411da1",
+    "url": "/static/media/redMana.8e7c6c62.png"
+  },
+  {
+    "revision": "e96a44a59b8f28711182fe47a7220e34",
+    "url": "/static/media/greenMana.e96a44a5.png"
+  },
+  {
+    "revision": "c17b6e5444dbdb65ebc65d809e3b2d24",
+    "url": "/static/media/boardBackground.c17b6e54.png"
+  },
+  {
+    "revision": "e8c8df1dc3df742b78a1f1bfe48315f4",
+    "url": "/static/media/green_background.e8c8df1d.jpg"
+  },
+  {
+    "revision": "912590e62e927a4fb3dfecbc0571e076",
+    "url": "/static/media/red_background.912590e6.png"
+  },
+  {
+    "revision": "8502a74c4a4f0a4fd6d0",
+    "url": "/static/js/main.8502a74c.chunk.js"
+  },
+  {
+    "revision": "68824b6d6b53363bce526ee542869309",
+    "url": "/static/media/Destroyed.68824b6d.mp3"
+  },
+  {
+    "revision": "42ade4fbe8a146b11d86055ae08573e0",
+    "url": "/static/media/Summon.42ade4fb.mp3"
+  },
+  {
+    "revision": "aa49b6085a7956abf7d14fda1332e3e9",
+    "url": "/static/media/EndTurn.aa49b608.mp3"
+  },
+  {
+    "revision": "889af84db3dbab30e62107c7a5382a6a",
+    "url": "/static/media/PlayerDamage.889af84d.mp3"
+  },
+  {
+    "revision": "61e969e826fd0c5e0565a95482199d73",
+    "url": "/static/media/Attack.61e969e8.mp3"
+  },
+  {
+    "revision": "9092c11340bf233b96eec62beda1d831",
+    "url": "/static/media/Defend.9092c113.mp3"
+  },
+  {
+    "revision": "a4696c80d97ff3c89407c5a1048564cf",
+    "url": "/static/media/Background.a4696c80.mp3"
+  },
+  {
+    "revision": "dc7c98754fc90acc9633c86eb2667b24",
+    "url": "/static/media/FireAvatar.dc7c9875.mp3"
+  },
+  {
+    "revision": "ab80e34227daaa3aad21066eda16a372",
+    "url": "/static/media/tile_bg_2.ab80e342.jpg"
+  },
+  {
+    "revision": "e25f28b349a2a86d228e2711d68cf73f",
+    "url": "/static/media/cardBack.e25f28b3.jpg"
+  },
+  {
+    "revision": "9beabf110a6f7246c116b66278d1b9b6",
+    "url": "/index.html"
+  }
+];
